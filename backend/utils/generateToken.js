3c09c16955218264395de3443f69db1ef188f723
Ahmed Yimer <100967941+ahmed0927626158@users.noreply.github.com>
2024-06-28 16:13:04 +0300

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-  const token = jwt.sign({ userId }, "zbzc32bV404v0tW4Jty1wbL1NPqQmvL5KJ+eP5J45p8=;", { expiresIn: "1d" });
+  const token = jwt.sign({ userId },process.env.SECRET_KEY, { expiresIn: "1d" });
 
   res.cookie("jwt", token, {
     maxAge: 3 * 24 * 60 * 60 * 1000,
