0ed63130dcc115366dfd36cb786277bca8a471c8
Ahmed Yimer <100967941+ahmed0927626158@users.noreply.github.com>
2024-06-28 14:12:13 +0300

    Update connectToMongoDB.js

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect("mongodb+srv://ahmed:ahmed1234zehara@cluster0.txyzm7h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("connected");
   } catch (error) {
     console.log("db connecting error", error);
