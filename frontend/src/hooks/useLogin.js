a081643dcf7b60092acdb960aaa52b202f3c7aed
Ahmed Yimer <100967941+ahmed0927626158@users.noreply.github.com>
2024-06-29 12:15:07 +0300

    Update useLogin.js

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -11,7 +11,7 @@ const  useLogin=()=>{
     try 
     {
        console.log(JSON.stringify({username,password}))
-        const response=await fetch("/api/auth/login/",{
+        const response=await fetch("https://mern-chat-app-client-kappa.vercel.app/api/auth/login/",{
             method:"POST",
             headers:{"Content-Type":"application/json"},
             body:JSON.stringify({username,password})
