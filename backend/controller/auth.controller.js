929f21f196b6a15f8146b0250806a7674c3588c6
Ahmed Yimer <100967941+ahmed0927626158@users.noreply.github.com>
2024-06-29 10:14:02 +0300

    Update auth.controller.js

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -51,18 +51,6 @@ export const signup = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-
- res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', 'https://mern-chat-app-client-kappa.vercel.app/')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-
-  
   try {
     const { username, password } = req.body;
   
