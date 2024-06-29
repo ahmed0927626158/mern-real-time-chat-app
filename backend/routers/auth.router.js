22b7890409b88c25c87fe8b54ec10ab7a0649fc9
Ahmed Yimer <100967941+ahmed0927626158@users.noreply.github.com>
2024-06-29 10:29:43 +0300

    Update auth.router.js

--- backend/routers/auth.router.js
+++ backend/routers/auth.router.js
@@ -1,8 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { login, signup, logout } from "./../controller/auth.controller.js";
 const router = express.Router();
-
-router.post("/signup", signup);
+var corsOptions = {
+  origin: 'https://mern-chat-app-client-kappa.vercel.app',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+router.post("/signup", cors(corsOptions),signup);
 router.get("/test",(req,res)=>{
   res.send(process.env.SECRET_KEY)
 })
