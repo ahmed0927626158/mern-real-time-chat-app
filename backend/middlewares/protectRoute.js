52cdbf5fa42cafeca8fc10ad40510cde7a3d1d96
Ahmed Yimer <100967941+ahmed0927626158@users.noreply.github.com>
2024-06-29 22:41:32 +0300

    Update protectRoute.js

--- backend/middlewares/protectRoute.js
+++ backend/middlewares/protectRoute.js
@@ -5,15 +5,15 @@ const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
     if (!token) {
-      return res.status(401).json({ message: "Unauthorized user" });
+      return res.status(401).json({ message: "Unauthorized user1" });
     }
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
     if (!decoded) {
-      return res.status(401).json({ message: "Unauthorized user" });
+      return res.status(401).json({ message: "Unauthorized user2" });
     }
     const user = await User.findById(decoded.userId).select("-password");
     if (!user) {
-      return res.status(401).json({ mesage: "Unauthorized user" });
+      return res.status(401).json({ mesage: "Unauthorized user3" });
     }
     req.user = user;
     console.log("from middleware",req.user)
