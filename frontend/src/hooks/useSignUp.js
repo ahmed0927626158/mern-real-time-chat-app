be3d3d31a8fefb9b28b695b0b2da8979a40b92de
Ahmed Yimer <100967941+ahmed0927626158@users.noreply.github.com>
2024-07-01 14:14:22 +0300

    Update useSignUp.js

--- frontend/src/hooks/useSignUp.js
+++ frontend/src/hooks/useSignUp.js
@@ -14,7 +14,8 @@ try {
     const response=await fetch("https://mern-chat-app-server-lovat.vercel.app/api/auth/signup",{
         method:"POST",
         headers:{"Content-Type":"application/json"},
-        body:JSON.stringify({fullName,username,password,confirm,gender,phone})
+        body:JSON.stringify({fullName,username,password,confirm,gender,phone}),
+        credentials:'include'
     })
     const data=await response.json()
     if(data.error)
