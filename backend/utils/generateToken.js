a9a25ed46564ef5159da3c2e729b5c14e502ce0a
Ahmed Yimer <100967941+ahmed0927626158@users.noreply.github.com>
2024-07-01 09:15:20 +0300

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,10 +4,10 @@ export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId },process.env.SECRET_KEY, { expiresIn: "1d" });
 
   res.cookie("jwt", token, {
+    path:'/'
     maxAge: 3 * 24 * 60 * 60 * 1000,
     httpOnly: true,//prevent XSS attacks cros-site scripting attacks
-    secure:'auto',
-    sameSite: "none", // CSRF attack cross-site request forgery
+  
   });
 };
 
