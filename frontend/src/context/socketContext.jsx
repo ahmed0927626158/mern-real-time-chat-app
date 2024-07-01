45f5f72152b57a0cbe3e1ab42d5f6732134700b9
Ahmed Yimer <100967941+ahmed0927626158@users.noreply.github.com>
2024-07-01 15:52:09 +0300

    Update socketContext.jsx

--- frontend/src/context/socketContext.jsx
+++ frontend/src/context/socketContext.jsx
@@ -16,6 +16,7 @@ export const SocketContextProvider=({children})=>{
                 query:{
                     userId:authUser.id
                 },
+                credentials:'include' 
 
             });
             setSocket(socket);
