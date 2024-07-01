c8d953db33465429eb3406e7957046f0321a8990
Ahmed Yimer <100967941+ahmed0927626158@users.noreply.github.com>
2024-07-01 15:57:45 +0300

    Update socketContext.jsx

--- frontend/src/context/socketContext.jsx
+++ frontend/src/context/socketContext.jsx
@@ -16,6 +16,7 @@ export const SocketContextProvider=({children})=>{
                 query:{
                     userId:authUser.id
                 },
+                transports: ['polling', 'websocket'],
                 credentials:'include' 
 
             });
