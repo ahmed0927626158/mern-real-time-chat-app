417a8950b3eaceabc86028ccbd3fcfa602375589
Ahmed Yimer <100967941+ahmed0927626158@users.noreply.github.com>
2024-07-01 10:34:35 +0300

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,6 +4,7 @@ export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId },process.env.SECRET_KEY, { expiresIn: "1d" });
 
   res.cookie("jwt", token, {
+    domain:"https://mern-chat-app-client-kappa.vercel.app",
     maxAge: 3 * 24 * 60 * 60 * 1000,
     httpOnly: true,//prevent XSS attacks cros-site scripting attacks
     sameSite:"none",
