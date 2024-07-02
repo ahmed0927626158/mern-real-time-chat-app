6a3792e5736bf4cb0ec144270e0dced0e7368491
Ahmed Yimer <100967941+ahmed0927626158@users.noreply.github.com>
2024-07-02 08:47:30 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,11 @@ import messageRoute from "./routers/message.router.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-
+app.use(cors({
+   origin: 'https://mern-chat-app-client-kappa.vercel.app',
+    credentials: true
+}
+));
 
 // Middlewares
 
